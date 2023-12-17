522d123a709973f083da3bfc7026d6cd18307130
marzieh <marziehmonshizade@yahoo.com>
2023-12-18 02:58:44 +0330

    add Link to sidebar

--- components/modules/sidebar/sidebar.js
+++ components/modules/sidebar/sidebar.js
@@ -1,11 +1,22 @@
+import Link from "next/link";
+
 export default function Sidebar() {
   return (
     <>
-      <ul className="p-3">
-        <li className="py-5">صفحه ی اصلی</li>
-        <li className="py-5"> تماس با ما </li>
-        <li className="py-5">درباره ی ما</li>
-        <li className="py-5 border-t-2 border-zinc-400"> خروج </li>
+      <ul className="md:p-3 p-1">
+        <li className="text-2xl font-bold tracking-tight">داشبورد</li>
+        <li className="py-5">
+          <Link href="">صفحه ی اصلی</Link>
+        </li>
+        <li className="py-5">
+          <Link href=""> تماس با ما </Link>
+        </li>
+        <li className="py-5">
+          <Link href="">درباره ی ما </Link>
+        </li>
+        <li className="py-5 border-t-2 border-gray-300">
+          <Link href=""> خروج</Link>
+        </li>
       </ul>
     </>
   );
