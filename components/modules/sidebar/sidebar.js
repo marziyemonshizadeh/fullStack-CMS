ffe3d8b01580e326146cc52199dad7bc6b07b154
marzieh <marziehmonshizade@yahoo.com>
2023-12-17 21:48:26 +0330

    coding sidebar

---
+++ components/modules/sidebar/sidebar.js
@@ -0,0 +1,12 @@
+export default function Sidebar() {
+  return (
+    <>
+      <ul className="p-3">
+        <li className="py-5">صفحه ی اصلی</li>
+        <li className="py-5"> تماس با ما </li>
+        <li className="py-5">درباره ی ما</li>
+        <li className="py-5 border-t-2 border-zinc-400"> خروج </li>
+      </ul>
+    </>
+  );
+}
