fd8b4750144746c3d1e7a6f3fb7598824aa336b5
marzieh <marziehmonshizade@yahoo.com>
2023-12-19 21:17:43 +0330

    coding responsive sidebar

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,14 @@
 import Navbar from "@/components/modules/navbar/navbar";
-import Sidebar from "@/components/modules/sidebar/sidebar.js";
+import Sidebar from "@/components/modules/sidebar/sidebar";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <div dir="rtl" className="grid grid-cols-6">
-      <div className="col-span-1 h-screen bg-slate-100">
+      <div className="md:col-span-1 col-span-6 bg-slate-100 md:h-screen min-h-32 sticky top-0 z-20">
         <Sidebar />
       </div>
-      <div className="col-span-5 p-4">
+      <div className="md:col-span-5 col-span-6 p-4">
         <Navbar />
         <Component {...pageProps} />
       </div>
