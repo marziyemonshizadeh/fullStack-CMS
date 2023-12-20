0b57d58a53c365c7c83bea366d2d13c9dc729ec3
marzieh <marziehmonshizade@yahoo.com>
2023-12-20 21:07:44 +0330

    coding edit course modal

--- components/modules/courseCard/courseCard.js
+++ components/modules/courseCard/courseCard.js
@@ -1,38 +1,47 @@
 import Link from "next/link";
 
-export default function CourseCard({ src, title }) {
+export default function CourseCard({
+  src,
+  title,
+  closeDeleteCourseModal,
+  closeEditCourseModal,
+}) {
   return (
-    <Link
-      href="#"
-      className="flex flex-row justify-between items-center bg-white border border-gray-200 rounded-lg  w-full my-4 shadow-lg hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
-    >
-      <div className="flex items-center">
-        {/* image */}
-        <img
-          className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
-          src={src}
-          alt="course"
-        />
-        {/* title */}
-        <h5 className="mb-2 text-2xl font-bold tracking-tight mx-4 text-gray-900 dark:text-white">
-          {title}
-        </h5>
-      </div>
-      {/* btns */}
-      <div>
-        <button
-          type="button"
-          class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-        >
-          حذف
-        </button>
-        <button
-          type="button"
-          class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-        >
-          ویرایش
-        </button>
-      </div>
-    </Link>
+    <>
+      <Link
+        href="#"
+        className="flex md:flex-row flex-col justify-between items-center bg-white border border-gray-200 rounded-lg  w-full my-4 shadow-lg hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+      >
+        <section className="md:flex md:items-center">
+          {/* image */}
+          <img
+            className="object-fill h-auto max-w-full  md:rounded-r-lg  md:h-auto md:w-48 md:rounded-none  md:rounded-s-lg"
+            src={src}
+            alt="course"
+          />
+          {/* title */}
+          <h5 className="mb-2 text-2xl text-center font-bold tracking-tight mx-4 text-gray-600 dark:text-white">
+            {title}
+          </h5>
+        </section>
+        {/* btns */}
+        <section className="m-4">
+          <button
+            type="button"
+            className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+            onClick={() => closeDeleteCourseModal()}
+          >
+            حذف
+          </button>
+          <button
+            type="button"
+            className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+            onClick={() => closeEditCourseModal()}
+          >
+            ویرایش
+          </button>
+        </section>
+      </Link>
+    </>
   );
 }
---
+++ components/modules/editCourseModal/editCourseModal.js
@@ -0,0 +1,62 @@
+import { useForm } from "react-hook-form";
+import "react-toastify/dist/ReactToastify.css";
+import ModalsTemplate from "../modalsTemplate/modalsTemplate";
+
+export default function EditCourseModal({ closeEditCourseModal }) {
+  const {
+    register,
+    reset,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = async (data) => {
+    await fetch("api/courses", {
+      method: "PUT",
+      headers: {
+        "content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then((res) => {
+      console.log("put res =>", res);
+      closeEditCourseModal();
+      reset();
+      return res.data;
+    });
+  };
+  return (
+    <ModalsTemplate>
+      <form className="max-w-sm mx-auto my-5" onSubmit={handleSubmit(onSubmit)}>
+        <header className="text-2xl font-bold tracking-tight text-center mb-7">
+          لطفا اطلاعات دوره ی خود را وارد کنید
+        </header>
+        <input
+          type="text"
+          id="text"
+          placeholder="عنوان جدید دوره"
+          className="bg-gray-50 border mb-5 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          {...register("title", { required: true })}
+        />
+        {errors.title && (
+          <p className="text-red-700">لطفا عنوان جدید دوره را وارد کنید !</p>
+        )}
+
+        <button
+          type="submit"
+          className="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+        >
+          اضافه کردن
+        </button>
+        <button
+          type="submit"
+          className="text-white bg-gradient-to-r from-slate-500 via-slate-600 to-slate-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-slate-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+          onClick={() => {
+            closeEditCourseModal();
+            reset();
+          }}
+        >
+          لغو
+        </button>
+      </form>
+    </ModalsTemplate>
+  );
+}
