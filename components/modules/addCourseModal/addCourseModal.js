38f462fe4133a4270087acdba9dfea177f776b29
marzieh <marziehmonshizade@yahoo.com>
2023-12-20 21:06:16 +0330

    coding add course modal

---
+++ components/modules/addCourseModal/addCourseModal.js
@@ -0,0 +1,127 @@
+import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import ModalsTemplate from "../modalsTemplate/modalsTemplate";
+export default function AddCourseModal({ closeAddCourseModal }) {
+  const {
+    register,
+    reset,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = async (data) => {
+    await fetch("api/courses", {
+      method: "POST",
+      headers: {
+        "content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then((res) => {
+      if (res.status === 201) {
+        reset();
+        setShowAddCourseModal((prev) => !prev);
+        toast.success("! دوره ی مورد نظر با موفقیت اضافه شد", {
+          position: "bottom-left",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      } else if (res.status === 422) {
+        toast.warning("! داده ی شما معتبر نمی باشد", {
+          position: "bottom-left",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      } else if (res.status === 404) {
+        toast.error("!  خطای سمت سرور رخ داده است", {
+          position: "bottom-left",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      }
+      return res.data;
+    });
+  };
+  return (
+    <ModalsTemplate>
+      <form className="max-w-sm mx-auto my-5" onSubmit={handleSubmit(onSubmit)}>
+        <header className="text-2xl font-bold tracking-tight text-center mb-7">
+          لطفا اطلاعات دوره ی خود را وارد کنید
+        </header>
+        <input
+          type="text"
+          id="text"
+          placeholder="عنوان دوره"
+          className="bg-gray-50 border mb-5 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          {...register("title", { required: true })}
+        />
+        {errors.title && (
+          <p className="text-red-700">لطفا عنوان دوره را وارد کنید !</p>
+        )}
+
+        <input
+          type="text"
+          id="text"
+          placeholder="آدرس تصویر مورد نظر را وارد کنید"
+          className="bg-gray-50 border mb-5 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          {...register("img", { required: true })}
+        />
+        {errors.img && (
+          <p className="text-red-700">لطفا آدرس تصویر را وارد کنید !</p>
+        )}
+
+        <input
+          type="text"
+          id="text"
+          placeholder=" لطفا قیمت دوره را وارد کنید"
+          className="bg-gray-50 border mb-5 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          {...register("price", { required: true })}
+        />
+        {errors.price && (
+          <p className="text-red-700">لطفا آدرس تصویر را وارد کنید !</p>
+        )}
+        <input
+          type="text"
+          id="text"
+          placeholder=" لطفا نام مدرس دوره را وارد کنید"
+          className="bg-gray-50 border mb-5 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          {...register("teacher", { required: true })}
+        />
+        {errors.teacher && (
+          <p className="text-red-700">! لطفا نام مدرس دوره را وارد کنید</p>
+        )}
+
+        <button
+          type="submit"
+          className="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+        >
+          اضافه کردن
+        </button>
+        <button
+          type="submit"
+          className="text-white bg-gradient-to-r from-slate-500 via-slate-600 to-slate-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-slate-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+          onClick={() => {
+            closeAddCourseModal();
+            reset();
+          }}
+        >
+          لغو
+        </button>
+      </form>
+    </ModalsTemplate>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,130 +1,60 @@
+import AddCourseModal from "@/components/modules/addCourseModal/addCourseModal";
 import CourseCard from "@/components/modules/courseCard/courseCard";
-import ModalsTemplate from "@/components/modules/modalsTemplate/modalsTemplate";
+import DeleteCourseModal from "@/components/modules/deleteCourseModal/deleteCourseModal";
+import EditCourseModal from "@/components/modules/editCourseModal/editCourseModal";
 import Title from "@/components/template/index/title";
 import coursesModel from "@/models/course.js";
 import connectToDB from "@/utils/db";
 import { useState } from "react";
-import { useForm } from "react-hook-form";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Home() {
   const [showAddCourseModal, setShowAddCourseModal] = useState(false);
+  const closeAddCourseModal = () => setShowAddCourseModal((prev) => !prev);
   const [showDeleteCourseModal, setShowDeleteCourseModal] = useState(false);
-  const {
-    register,
-    reset,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-  const onSubmit = (data) => {
-    console.log(data);
-    setShowAddCourseModal((prev) => !prev);
-    // axios.post("http://localhost:4001/Estates", data).then((res) => {
-    //   console.log("get post called");
-    //   toast.success("! ملک مورد نظر با موفقیت اضافه شد", {
-    //     position: "bottom-left",
-    //     autoClose: 5000,
-    //     hideProgressBar: false,
-    //     closeOnClick: true,
-    //     pauseOnHover: true,
-    //     draggable: true,
-    //     progress: undefined,
-    //     theme: "light",
-    //   });
-    //   return res.data;
-    // });
-    reset();
-  };
+  const closeDeleteCourseModal = () =>
+    setShowDeleteCourseModal((prev) => !prev);
+  const [showEditCourseModal, setShowEditCourseModal] = useState(false);
+  const closeEditCourseModal = () => setShowEditCourseModal((prev) => !prev);
   return (
     <main>
       <Title setShowAddCourseModal={setShowAddCourseModal} />
-
+      <ToastContainer />
       {showAddCourseModal && (
-        <ModalsTemplate>
-          <form
-            className="max-w-sm mx-auto my-5"
-            onSubmit={handleSubmit(onSubmit)}
-          >
-            <header className="text-2xl font-bold tracking-tight text-center mb-7">
-              لطفا اطلاعات دوره ی خود را وارد کنید
-            </header>
-            <input
-              type="text"
-              id="text"
-              placeholder="عنوان دوره"
-              className="bg-gray-50 border mb-5 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              {...register("title", { required: true })}
-            />
-            {errors.title && (
-              <p className="text-red-700">لطفا عنوان دوره را وارد کنید !</p>
-            )}
-
-            <input
-              type="text"
-              id="text"
-              placeholder="آدرس تصویر مورد نظر را وارد کنید"
-              className="bg-gray-50 border mb-5 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              {...register("img", { required: true })}
-            />
-            {errors.img && (
-              <p className="text-red-700">لطفا آدرس تصویر را وارد کنید !</p>
-            )}
-
-            <button
-              type="submit"
-              className="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-            >
-              اضافه کردن
-            </button>
-            <button
-              type="submit"
-              className="text-white bg-gradient-to-r from-slate-500 via-slate-600 to-slate-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-slate-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-              onClick={() => {
-                setShowAddCourseModal((prev) => !prev);
-              }}
-            >
-              لغو
-            </button>
-          </form>
-        </ModalsTemplate>
+        <AddCourseModal closeAddCourseModal={closeAddCourseModal} />
       )}
+      {console.log("showDeleteCourseModal", showDeleteCourseModal)}
       {showDeleteCourseModal && (
-        <ModalsTemplate>
-          <div className="text-center">
-            <h2 className="text-2xl font-bold tracking-tight mb-5">
-              آیا مطمئن هستید می خواهید دوره را حذف کنید ؟
-            </h2>
-            <button
-              type="button"
-              className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-              onClick={() => setShowDeleteCourseModal((prev) => !prev)}
-            >
-              خیر
-            </button>
-            <button
-              type="button"
-              className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-            >
-              بله
-            </button>
-          </div>
-        </ModalsTemplate>
+        <DeleteCourseModal closeDeleteCourseModal={closeDeleteCourseModal} />
+      )}
+      {showEditCourseModal && (
+        <EditCourseModal closeEditCourseModal={closeEditCourseModal} />
       )}
 
       <CourseCard
         src="https://faradars.org/wp-content/uploads/2018/03/fvds9612-png.png"
         title="algoritm"
+        closeDeleteCourseModal={closeDeleteCourseModal}
+        closeEditCourseModal={closeEditCourseModal}
       />
       <CourseCard
         src="https://files.virgool.io/upload/users/89185/posts/kulesofpxkl6/zxek9hmagsff.png"
         title="mongo db"
+        closeDeleteCourseModal={closeDeleteCourseModal}
+        closeEditCourseModal={closeEditCourseModal}
       />
       <CourseCard
         src="https://mobisoftinfotech.com/resources/wp-content/uploads/2022/04/next-JS-framework.png"
         title="next js"
+        closeDeleteCourseModal={closeDeleteCourseModal}
+        closeEditCourseModal={closeEditCourseModal}
       />
       <CourseCard
         src="https://media.geeksforgeeks.org/wp-content/uploads/20231211135520/Tailwind-CSS.webp"
         title="tailwind css"
+        closeDeleteCourseModal={closeDeleteCourseModal}
+        closeEditCourseModal={closeEditCourseModal}
       />
     </main>
   );
