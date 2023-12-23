be97facffef13efa3b8b6d641a925c8c88185bfc
marzieh <marziehmonshizade@yahoo.com>
2023-12-24 01:12:37 +0330

    coding delete and put course api

---
+++ pages/api/courses/[id].js
@@ -0,0 +1,37 @@
+import coursesModel from "@/models/course";
+import connectToDB from "@/utils/db";
+import { isValidObjectId } from "mongoose";
+
+const handler = async (req, res) => {
+  connectToDB();
+
+  if (req.method === "DELETE") {
+    const { id } = req.query;
+
+    if (isValidObjectId(id)) {
+      try {
+        await coursesModel.findOneAndDelete({ _id: id });
+        return res.json({ message: "Course Removed Successfully :))" });
+      } catch (err) {
+        return res.status(500).json({ message: "Internal server error !!" });
+      }
+    } else {
+      return res.status(422).json({ message: "Course ID is not valid !!" });
+    }
+  } else if (req.method === "PUT") {
+    const { id } = req.query;
+    const { title } = req.body;
+    if (isValidObjectId(id)) {
+      try {
+        await coursesModel.findOneAndUpdate({ _id: id }, { title });
+        return res.json({ message: "Course Updated Successfully :))" });
+      } catch (err) {
+        return res.status(500).json({ message: "Internal server error !!" });
+      }
+    } else {
+      return res.status(422).json({ message: "Course ID is not valid !!" });
+    }
+  }
+};
+
+export default handler;
