9000fd8ca32a2e4f3a7e5a878d5f175fbd7479a8
marzieh <marziehmonshizade@yahoo.com>
2023-12-26 02:44:53 +0330

    refactor delete course modal

--- components/template/index/deleteCourseModal/deleteCourseModal.js
+++ components/template/index/deleteCourseModal/deleteCourseModal.js
@@ -1,6 +1,9 @@
 import ModalsTemplate from "../../../modules/modalsTemplate/modalsTemplate";
 
-export default function DeleteCourseModal({ closeDeleteCourseModal }) {
+export default function DeleteCourseModal({
+  removeHandler,
+  closeDeleteCourseModal,
+}) {
   return (
     <>
       <ModalsTemplate>
@@ -11,7 +14,10 @@ export default function DeleteCourseModal({ closeDeleteCourseModal }) {
           <button
             type="button"
             className="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-            onClick={() => closeDeleteCourseModal()}
+            onClick={() => {
+              removeHandler();
+              closeDeleteCourseModal();
+            }}
           >
             بله
           </button>
