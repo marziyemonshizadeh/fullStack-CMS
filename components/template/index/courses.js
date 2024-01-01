f3eb273f63e0b863079ed38332cce3948df79a0c
marzieh <marziehmonshizade@yahoo.com>
2024-01-01 16:58:51 +0330

    use PagesHeader component

--- components/template/index/courses.js
+++ components/template/index/courses.js
@@ -1,4 +1,5 @@
 import CourseCard from "@/components/modules/courseCard/courseCard";
+import PagesHeader from "@/components/modules/pagesHeader/pagesHeader";
 import { useState } from "react";
 import AddCourseModal from "./addCourseModal/addCourseModal";
 
@@ -21,17 +22,12 @@ export default function Courses({ courses }) {
   };
   return (
     <>
-      {/* ADD COURSE BTN */}
-      <div className="flex justify-between my-4">
-        <h1 className="text-2xl font-bold tracking-tight">دوره ها</h1>
-        <button
-          type="button"
-          className="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-          onClick={() => setShowAddCourseModal((prev) => !prev)}
-        >
-          اضافه کردن دوره ی جدید
-        </button>
-      </div>
+      <PagesHeader
+        title="دوره ها"
+        btnText="اضافه کردن دوره ی جدید"
+        clickHandler={closeAddCourseModal}
+      />
+
       {/* COURSES */}
       {data?.map((item) => {
         return (
