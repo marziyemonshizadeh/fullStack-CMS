7e67f42cd691ee45996bc542098620d6ed32d03d
marzieh <marziehmonshizade@yahoo.com>
2024-01-11 02:08:07 +0330

    coding teacher api

---
+++ pages/api/teachers/index.js
@@ -0,0 +1,30 @@
+const { default: connectToDB } = require("@/utils/db");
+import TeacherModel from "@/models/teacher";
+
+const handler = async (req, res) => {
+  connectToDB();
+  if (req.method === "GET") {
+    console.log("req.query=", req.query);
+    if (req.query.q) {
+      const { q } = req.query;
+      const teachers = await TeacherModel.find({ name: { $regex: q } });
+      res.json(teachers);
+    } else {
+      const teachers = await TeacherModel.find({});
+      return res.json(teachers);
+    }
+  } else if (req.method === "POST") {
+    try {
+      const { name } = req.body;
+      // title.trim() === '' mosavi ba !title.trim()
+      if (!name.trim() || name.length < 5) {
+        return res.status(422).json({ message: "title is not valid !!!" });
+      }
+      await TeacherModel.create({ name });
+      return res.status(201).json({ message: "teacher created successfully" });
+    } catch (err) {
+      return res.status(500).json({ message: "internal server error !!!" });
+    }
+  }
+};
+export default handler;
