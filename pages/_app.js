7000bb5f8ad5eb8594083c22ee301959be5609e0
marzieh <marziehmonshizade@yahoo.com>
2024-01-23 19:42:55 +0330

    add select-none class

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
-    <div dir="rtl" className="grid grid-cols-6">
+    <div dir="rtl" className="grid grid-cols-6 select-none">
       <div className="md:col-span-1 col-span-6 bg-slate-100 md:h-screen min-h-32 sticky top-0 z-20">
         <Sidebar />
       </div>
