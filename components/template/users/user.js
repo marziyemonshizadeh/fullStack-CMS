0db9a4f67af15e4ef07f333b47810f20c5b8dabd
marzieh <marziehmonshizade@yahoo.com>
2024-01-23 19:42:06 +0330

    coding user page

--- components/template/users/user.js
+++ components/template/users/user.js
@@ -1,30 +1,51 @@
-export default function User() {
+import PagesHeader from "@/components/modules/pagesHeader/pagesHeader";
+import UserCard from "@/components/modules/userCard/userCard";
+import { useState } from "react";
+import AddCourseModal from "../index/addCourseModal/addCourseModal";
+
+export default function User({ users }) {
+  const [showAddUserModal, setShowAddUserModal] = useState(false);
+  const closeAddUserModal = () => setShowAddUserModal((prev) => !prev);
+
+  //  for make real time => later use context
+  const [data, setData] = useState([...users]);
+  const getUsers = async () => {
+    const res = await fetch(`/api/users`);
+    const coursesData = await res.json();
+
+    console.log("Res =>", res);
+
+    if (res.status === 200) {
+      console.log(coursesData);
+      setData(coursesData);
+    }
+  };
+
   return (
-    <div className="flex md:flex-row flex-col justify-between items-center bg-white border border-gray-200 rounded-lg  w-full my-4 p-4 shadow-lg hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-      <div className="flex md:flex-row flex-col items-center gap-4">
-        <img
-          src="https://pics.craiyon.com/2023-06-20/89f79a8dee744596981e7417b8a7ea1d.webp"
-          alt="user"
-          className="rounded-full w-12"
+    <>
+      <PagesHeader
+        title="کاربران"
+        btnText="اضافه کردن  کاربر جدید"
+        clickHandler={closeAddUserModal}
+      />
+
+      {data?.map((i) => {
+        return (
+          <UserCard
+            username={i.userName}
+            status={i.status}
+            purchases={i.purchases}
+            email={i.email}
+          />
+        );
+      })}
+      {/* MODALS */}
+      {showAddUserModal && (
+        <AddCourseModal
+          getCourses={getUsers}
+          closeAddCourseModal={closeAddUserModal}
         />
-        <p className="text-2xl text-center font-bold tracking-tight text-gray-600">
-          marziii
-        </p>
-      </div>
-      <section>
-        <button
-          type="button"
-          className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-        >
-          حذف
-        </button>
-        <button
-          type="button"
-          className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-        >
-          ویرایش
-        </button>
-      </section>
-    </div>
+      )}
+    </>
   );
 }
