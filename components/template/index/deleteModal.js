7b43909b1c7f5beaaf4a5ffad757531c274cc542
marzieh <marziehmonshizade@yahoo.com>
2024-02-29 00:54:09 +0330

    create reusable delete modal

--- components/template/index/deleteModal.js
+++ components/template/index/deleteModal.js
@@ -1,22 +1,19 @@
-import ModalsTemplate from "../../../modules/modalsTemplate/modalsTemplate";
+import ModalsTemplate from "../../modules/modalsTemplate/modalsTemplate";
 
-export default function DeleteCourseModal({
-  removeHandler,
-  closeDeleteCourseModal,
-}) {
+export default function DeleteCourseModal({ removeHandler, closeDeleteModal }) {
   return (
     <>
       <ModalsTemplate>
         <div className="text-center">
           <h2 className="text-2xl font-bold tracking-tight mb-5">
-            آیا مطمئن هستید می خواهید دوره را حذف کنید ؟
+            آیا مطمئن هستید می خواهید حذف کنید ؟
           </h2>
           <button
             type="button"
             className="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
             onClick={() => {
               removeHandler();
-              closeDeleteCourseModal();
+              closeDeleteModal();
             }}
           >
             بله
@@ -24,7 +21,7 @@ export default function DeleteCourseModal({
           <button
             type="button"
             className="text-white bg-gradient-to-r from-slate-500 via-slate-600 to-slate-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-slate-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-            onClick={() => closeDeleteCourseModal()}
+            onClick={() => closeDeleteModal()}
           >
             خیر
           </button>
