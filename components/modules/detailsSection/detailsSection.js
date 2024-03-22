699980b2b01f2d9a08698c7821a30a2bf90790fb
marzieh <marziehmonshizade@yahoo.com>
2024-03-22 20:22:37 +0330

    add font bold in DetailsSection component

--- components/modules/detailsSection/detailsSection.js
+++ components/modules/detailsSection/detailsSection.js
@@ -1,7 +1,7 @@
 export default function DetailsSection({ text, value }) {
   return (
     <section className="flex justify-between items-center gap-10 my-2">
-      <span>{text}</span>
+      <span className="font-bold">{text}</span>
       <div className="bg-slate-50 border-2 border-slate-100 rounded-xl p-3 w-2/3">
         {value}
       </div>
