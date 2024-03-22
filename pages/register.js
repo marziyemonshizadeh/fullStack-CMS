955268ce524b46296d066b97b715ea807909d938
marzieh <marziehmonshizade@yahoo.com>
2024-03-22 16:18:49 +0330

    coding sign up feature

--- pages/register.js
+++ pages/register.js
@@ -1,7 +1,10 @@
+import { useRouter } from "next/router";
 import { useForm } from "react-hook-form";
+import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function Register() {
+  const router = useRouter();
   const {
     register,
     reset,
@@ -12,143 +15,146 @@ export default function Register() {
     console.log("clicked");
     console.log(data);
     reset();
-    // await fetch("api/teachers", {
-    //   method: "POST",
-    //   headers: {
-    //     "content-Type": "application/json",
-    //   },
-    //   body: JSON.stringify(data),
-    // }).then((res) => {
-    //   if (res.status === 201) {
-    //     getTeachers();
-    //     toast.success("! استاد مورد نظر با موفقیت اضافه شد", {
-    //       position: "bottom-left",
-    //       autoClose: 5000,
-    //       hideProgressBar: false,
-    //       closeOnClick: true,
-    //       pauseOnHover: true,
-    //       draggable: true,
-    //       progress: undefined,
-    //       theme: "light",
-    //     });
-    //     reset();
-    //     closeAddTeacherModal();
-    //   } else if (res.status === 422) {
-    //     toast.warning("! داده ی شما معتبر نمی باشد", {
-    //       position: "bottom-left",
-    //       autoClose: 5000,
-    //       hideProgressBar: false,
-    //       closeOnClick: true,
-    //       pauseOnHover: true,
-    //       draggable: true,
-    //       progress: undefined,
-    //       theme: "light",
-    //     });
-    //   } else if (res.status === 404) {
-    //     toast.error("!  خطای سمت سرور رخ داده است", {
-    //       position: "bottom-left",
-    //       autoClose: 5000,
-    //       hideProgressBar: false,
-    //       closeOnClick: true,
-    //       pauseOnHover: true,
-    //       draggable: true,
-    //       progress: undefined,
-    //       theme: "light",
-    //     });
-    //   }
-    //   return res.data;
-    // });
+    await fetch("api/auth/signup", {
+      method: "POST",
+      headers: {
+        "content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then((res) => {
+      if (res.status === 201) {
+        console.log("ok");
+        toast.success("! کاربر مورد نظر با موفقیت اضافه شد", {
+          position: "bottom-left",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+        reset();
+        //chetor begam bad az 3 saniye bia in khat ro ejra kon?
+        router.replace("/");
+      } else if (res.status === 422) {
+        toast.warning("This username or email exist already !!", {
+          position: "bottom-left",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      } else if (res.status === 404) {
+        toast.error("!  خطای سمت سرور رخ داده است", {
+          position: "bottom-left",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      }
+      return res.data;
+    });
   };
   return (
-    <div class="max-w-[500px] mx-auto my-8">
+    <div className="max-w-[500px] mx-auto my-8">
       <form
-        class="bg-white shadow-md rounded w-full px-8 pt-6 pb-8 mb-4"
+        className="bg-white shadow-md rounded w-full px-8 pt-6 pb-8 mb-4"
         onSubmit={handleSubmit(onSubmit)}
       >
-        <div class="mb-4">
+        <ToastContainer />
+
+        <div className="mb-4">
           <label
-            class="block text-gray-700 text-sm font-bold mb-2"
+            className="block text-gray-700 text-sm font-bold mb-2"
             htmlFor="firstName"
           >
             نام
           </label>
           <input
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="firstName"
             type="text"
             placeholder="نام "
             {...register("firstName", { required: true })}
           />
         </div>
-        <div class="mb-4">
+        <div className="mb-4">
           <label
-            class="block text-gray-700 text-sm font-bold mb-2"
+            className="block text-gray-700 text-sm font-bold mb-2"
             htmlFor="lastName"
           >
             نام خانوادگی
           </label>
           <input
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="lastName"
             type="text"
             placeholder="نام خانوادگی"
             {...register("lastName", { required: true })}
           />
         </div>
-        <div class="mb-4">
+        <div className="mb-4">
           <label
-            class="block text-gray-700 text-sm font-bold mb-2"
+            className="block text-gray-700 text-sm font-bold mb-2"
             htmlFor="userName"
           >
             نام کاربری
           </label>
           <input
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="userName"
             type="text"
             placeholder="نام کاربری"
             {...register("userName", { required: true })}
           />
         </div>
-        <div class="mb-6">
+        <div className="mb-6">
           <label
-            class="block text-gray-700 text-sm font-bold mb-2"
+            className="block text-gray-700 text-sm font-bold mb-2"
             htmlFor="password"
           >
             رمز عبور
           </label>
           <input
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="password"
             type="password"
             placeholder="رمز عبور"
             {...register("password", { required: true })}
           />
         </div>
-        {/* <div class="mb-6">
+        <div className="mb-6">
           <label
-            class="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="status"
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="email"
           >
-            وضعیت
+            email
           </label>
           <input
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="status"
-            type="status"
-            placeholder="please write user or admin"
-            {...register("status", { required: true })}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="email"
+            type="email"
+            placeholder="email"
+            {...register("email", { required: true })}
           />
-        </div> */}
-        <div class="flex items-center justify-between">
+        </div>
+        <div className="flex items-center justify-between">
           <a
-            class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+            className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
             href="/login"
           >
             قبلا ثبت نام کرده اید /ورود
           </a>
           <button
-            class="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+            className="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
             type="submit"
           >
             ثبت نام
